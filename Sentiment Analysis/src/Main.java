090078694a72723712563a6489285fc2003bcc69
Fraser P. Newton <fpnewton90@gmail.com>
2013-07-09 11:22:29 -0400

    Reads sample 01.txt and splits it according to sentences.

--- Sentiment Analysis/src/Main.java
+++ Sentiment Analysis/src/Main.java
@@ -1,31 +1,47 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStream;
 
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
+
 
 public class Main
 {
 
 	public static void main(String[] args)
 	{
-		 String sampleText = "Hello, world!. This is a lovely phone.";
-		
-		 try
-		 {
-			 InputStream is = new FileInputStream(System.getProperty("user.dir") + System.getProperty("file.separator") + "models" + System.getProperty("file.separator") + "en-sent.bin");
-			 SentenceModel model = new SentenceModel(is);
-			 SentenceDetectorME sdetector = new SentenceDetectorME(model);
+		try
+		{
+			BufferedReader in = new BufferedReader(new FileReader(System.getProperty("user.dir") + System.getProperty("file.separator") + "samples" + System.getProperty("file.separator") + "01.txt"));
+			StringBuilder sampleText = new StringBuilder();
+			String line;
 			
-			 String sentences[] = sdetector.sentDetect(sampleText);
+			while((line = in.readLine()) != null)
+			{
+			    sampleText.append(line);
+			}
 			
-			 System.out.println(sentences[0]);
-			 System.out.println(sentences[1]);
-			 is.close();
-		 }
-		 catch (Exception e)
-		 {
-			 e.printStackTrace();
-		 }
+			in.close();
+			
+			
+			InputStream is = new FileInputStream(System.getProperty("user.dir") + System.getProperty("file.separator") + "models" + System.getProperty("file.separator") + "en-sent.bin");
+			SentenceModel model = new SentenceModel(is);
+			SentenceDetectorME sdetector = new SentenceDetectorME(model);
+
+			String sentences[] = sdetector.sentDetect(sampleText.toString());
+
+			for (int i = 0; i < sentences.length; i++)
+			{
+				System.out.println("" + i + ": " + sentences[i]);
+			}
+
+			is.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
