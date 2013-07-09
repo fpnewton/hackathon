f0598ce3271813ae5e2a2ab25e8f4262ed750975
pacom20003 <pacom20003@hotmail.com>
2013-07-09 12:48:35 -0700

    Used arrays for threads

--- Sentiment Analysis/src/Main.java
+++ Sentiment Analysis/src/Main.java
@@ -17,7 +17,7 @@ public class Main
 	{
 		try
 		{
-			String fileName = "Entire Raw Text.txt";
+			String fileName = "01.txt";
 			BufferedReader in = new BufferedReader(new FileReader(System.getProperty("user.dir") + System.getProperty("file.separator") + "samples" + System.getProperty("file.separator") + fileName));
 			String line;
 			
@@ -26,61 +26,70 @@ public class Main
 			
 			while (true)
 			{
-				ParserThread t1 = null;
-				ParserThread t2 = null;
-				ParserThread t3 = null;
-				ParserThread t4 = null;
-				ParserThread t5 = null;
-				ParserThread t6 = null;
-				ParserThread t7 = null;
-				ParserThread t8 = null;
+				/*ParserThread threads[1] = null;
+				ParserThread threads[2] = null;
+				ParserThread threads[3] = null;
+				ParserThread threads[4] = null;
+				ParserThread threads[5] = null;
+				ParserThread threads[6] = null;
+				ParserThread threads[7] = null;
+				ParserThread threads[8] = null;*/
+				
+				ParserThread[] threads = new ParserThread[8];
 				
 				if ((line = in.readLine()) != null)
 				{
-					t1 = new ParserThread(line);
+					threads[0] = new ParserThread(line);
+				}
+				if ((line = in.readLine()) != null)
+				{
+					threads[1] = new ParserThread(line);
 				}
 				
 				if ((line = in.readLine()) != null)
 				{
-					t2 = new ParserThread(line);
+					threads[2] = new ParserThread(line);
 				}
 				
 				if ((line = in.readLine()) != null)
 				{
-					t3 = new ParserThread(line);
+					threads[3] = new ParserThread(line);
 				}
 				
 				if ((line = in.readLine()) != null)
 				{
-					t4 = new ParserThread(line);
+					threads[4] = new ParserThread(line);
 				}
 				
 				if ((line = in.readLine()) != null)
 				{
-					t5 = new ParserThread(line);
+					threads[5] = new ParserThread(line);
 				}
 				
 				if ((line = in.readLine()) != null)
 				{
-					t6 = new ParserThread(line);
+					threads[6] = new ParserThread(line);
 				}
 				
 				if ((line = in.readLine()) != null)
 				{
-					t7 = new ParserThread(line);
+					threads[7] = new ParserThread(line);
 				}
 				
-				if ((line = in.readLine()) != null)
-				{
-					t8 = new ParserThread(line);
+				
+				
+				while ((threads[0] != null && threads[0].t.isAlive()) || (threads[1] != null && threads[1].t.isAlive()) || (threads[2] != null && threads[2].t.isAlive()) || (threads[3] != null && threads[3].t.isAlive()) || (threads[4] != null && threads[4].t.isAlive()) || (threads[5] != null && threads[5].t.isAlive()) || (threads[6] != null && threads[6].t.isAlive()) || (threads[7] != null && threads[7].t.isAlive())) {}
+				
+				for (ParserThread pt : threads) {
+					if (pt != null) {
+						positive += pt.positive;
+						negative += pt.negative;
+					}
 				}
+				//positive += threads[1].positive + threads[2].positive + threads[3].positive + threads[4].positive + threads[5].positive + threads[6].positive + threads[7].positive + threads[8].positive;
+				//negative += threads[1].negative + threads[2].negative + threads[3].negative + threads[4].negative + threads[5].negative + threads[6].negative + threads[7].negative + threads[8].negative;
 				
-				while ((t1 != null && t1.t.isAlive()) || (t2 != null && t2.t.isAlive()) || (t3 != null && t3.t.isAlive()) || (t4 != null && t4.t.isAlive()) || (t5 != null && t5.t.isAlive()) || (t6 != null && t6.t.isAlive()) || (t7 != null && t7.t.isAlive()) || (t8 != null && t8.t.isAlive())) {}
-				
-				positive += t1.positive + t2.positive + t3.positive + t4.positive + t5.positive + t6.positive + t7.positive + t8.positive;
-				negative += t1.negative + t2.negative + t3.negative + t4.negative + t5.negative + t6.negative + t7.negative + t8.negative;
-				
-				if (t1 == null && t2 == null && t3 == null && t4 == null && t5 == null && t6 == null && t7 == null && t8 == null)
+				if (threads[0] == null && threads[1] == null && threads[2] == null && threads[3] == null && threads[4] == null && threads[5] == null && threads[6] == null && threads[7] == null)
 				{
 					break;
 				}
