e3cd70abf836d141e7f72bdbcd0f40997895a34b
Fraser P. Newton <fpnewton90@gmail.com>
2013-07-09 10:53:24 -0400

    Added training files.

--- Sentiment Analysis/src/Main.java
+++ Sentiment Analysis/src/Main.java
@@ -4,28 +4,28 @@ import java.io.InputStream;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		 String sampleText = "Hello, world!. This is a lovely phone.";
 		
-
-		String sampleText = "This is a lovely phone.";
-		
-		try {
-			InputStream is = new FileInputStream("en-sent.bin");
-			SentenceModel model = new SentenceModel(is);
-			SentenceDetectorME sdetector = new SentenceDetectorME(model);
-	 
-			String sentences[] = sdetector.sentDetect(sampleText);
-	 
-			System.out.println(sentences[0]);
-			System.out.println(sentences[1]);
-			is.close();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+		 try
+		 {
+			 InputStream is = new FileInputStream(System.getProperty("user.dir") + System.getProperty("file.separator") + "models" + System.getProperty("file.separator") + "en-sent.bin");
+			 SentenceModel model = new SentenceModel(is);
+			 SentenceDetectorME sdetector = new SentenceDetectorME(model);
+			
+			 String sentences[] = sdetector.sentDetect(sampleText);
+			
+			 System.out.println(sentences[0]);
+			 System.out.println(sentences[1]);
+			 is.close();
+		 }
+		 catch (Exception e)
+		 {
+			 e.printStackTrace();
+		 }
 	}
-
 }
