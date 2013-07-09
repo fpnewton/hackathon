0e349b498a1aa8cf883f7eb4dba676c119744505
pacom20003 <pacom20003@hotmail.com>
2013-07-09 09:48:50 -0700

    Added Positive Words.

---
+++ Sentiment Analysis/src/sentiment/analysis/PositiveWords.java
@@ -0,0 +1,51 @@
+package sentiment.analysis;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+
+public class PositiveWords {
+
+	private String[] words;
+	
+	public PositiveWords() {
+		try
+		{
+			String fileName = "Positive Words.txt";
+			BufferedReader in = new BufferedReader(new FileReader(System.getProperty("user.dir") + System.getProperty("file.separator") + "samples" + System.getProperty("file.separator") + fileName));
+			
+			StringBuilder text = new StringBuilder();
+			String line;
+			while((line = in.readLine()) != null)
+			{
+			    text.append(line+"\n");
+			}
+			in.close();
+			
+			
+			InputStream is = new FileInputStream(System.getProperty("user.dir") + System.getProperty("file.separator") + "models" + System.getProperty("file.separator") + "en-token.bin");
+			TokenizerModel tokenModel = new TokenizerModel(is);
+			Tokenizer tokenizer = new TokenizerME(tokenModel);
+			words = tokenizer.tokenize(text.toString());
+//			for (String s : words)
+//				System.out.println(s);
+//			System.out.println(words.length);
+	 
+			is.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean contains(String phrase) {
+		return Arrays.asList(words).contains(phrase);
+	}
+}
