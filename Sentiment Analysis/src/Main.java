b45f5ce1c62e0ba4e5bfa86412c63d6be9928d89
pacom20003 <pacom20003@hotmail.com>
2013-07-09 07:22:17 -0700

    Updated Java files

--- Sentiment Analysis/src/Main.java
+++ Sentiment Analysis/src/Main.java
@@ -1,9 +1,31 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+		String sampleText = "This is a lovely phone.";
+		
+		try {
+			InputStream is = new FileInputStream("en-sent.bin");
+			SentenceModel model = new SentenceModel(is);
+			SentenceDetectorME sdetector = new SentenceDetectorME(model);
+	 
+			String sentences[] = sdetector.sentDetect(sampleText);
+	 
+			System.out.println(sentences[0]);
+			System.out.println(sentences[1]);
+			is.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- Sentiment Analysis/src/sentiment/analysis/RelativeFileReader.java
+++ Sentiment Analysis/src/sentiment/analysis/RelativeFileReader.java
@@ -1,9 +1,31 @@
 package sentiment.analysis;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class RelativeFileReader {
+	
+	private String filePath;
 	
 	public RelativeFileReader(String filePath) {
 		
+	}
+	
+	public InputStream getResource() {
+		try {
+			// try relative to current directory
+			File file = new File(filePath);
+			if ( file.exists() ) {
+				return new FileInputStream( file );
+			}
+			// try the classpath
+			return Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
